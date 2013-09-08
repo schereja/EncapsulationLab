7b4f0399a7c773bb5b7f4d2bf1a0727d6538f43a
schereja <schereja@10.30.17.193>
2013-09-08 18:47:34 -0500

    Added more features to lab 2

--- src/lab2/GameGUI.java
+++ src/lab2/GameGUI.java
@@ -9,5 +9,46 @@ package lab2;
  * @author schereja
  */
 public class GameGUI {
+    private int move = 0;
     
+    public void move(int move){
+        if(move == 0){
+         rightMove();
+        }
+        else if (move==1){
+         leftMove();
+        }
+      
+      
+    }
+    public void attack(int attack){
+        switch(attack){
+            case 1:cut();
+                   break;
+            case 2:stab();
+                   break;
+        }
+        
+    }
+    public void inventory(){
+        
+    }
+    private String rightMove(){
+        return "Right";
+    }
+    private String leftMove(){
+        return "Left";
+    }
+    private String forwardMove(){
+        return "Forward";
+    }
+    private String reverseMove(){
+        return "Reverse";
+    }
+    private String cut(){
+        return "You have been cut.";
+    }
+    private String stab(){
+        return "You have been stabbed.";
+    }
 }
